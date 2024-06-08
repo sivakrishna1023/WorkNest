e8667f919bc9f09ad7419188549268a1dccabbea
Sainath <yuvasainath85@gmail.com>
2024-06-08 16:56:16 +0530

    updated cors

--- Backend/index.js
+++ Backend/index.js
@@ -14,8 +14,11 @@ app.use(cors({
     origin:"https://work-nest-omega.vercel.app",
     methods:["GET","POST","PUT","DELETE"],
     credentials:true
+    },{
+        origin:"http://localhost:3000",
+        methods:["GET","POST","PUT","DELETE"],
+        credentials:true
     }
-
 ));
 app.use(express.json());
 
