8ab7921467f015f614302013656588bf7db6b676
Sainath <yuvasainath85@gmail.com>
2024-06-08 16:57:16 +0530

    updated cors

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ app.use(cors({
     methods:["GET","POST","PUT","DELETE"],
     credentials:true
     },{
-        origin:"http://localhost:3000",
+        origin:"http://localhost:3001",
         methods:["GET","POST","PUT","DELETE"],
         credentials:true
     }
